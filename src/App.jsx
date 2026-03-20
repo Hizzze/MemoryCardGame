b52df4d2cb60168761ce955a13412504bad726dc
Vlad Syzov <hizes5050@gmail.com>
2026-03-20 14:06:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { GameHeader } from './components/GameHeader';
+
+const cardValues = [
+  '🍎',
+  '🍌',
+  '🍇',
+  '🍊',
+  '🍓',
+  '🥝',
+  '🍑',
+  '🍒',
+  '🍎',
+  '🍌',
+  '🍇',
+  '🍊',
+  '🍓',
+  '🥝',
+  '🍑',
+  '🍒',
+];
+
+function App() {
+  return ( 
+    <div className="app">
+      <GameHeader score={3} moves={10} />
+
+      <div className='cards-grid'>
+        {cardValues.map((card) => {
+          
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
