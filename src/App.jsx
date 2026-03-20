db622729410516f685a5f465f7eeb065697ceeb5
Vlad Syzov <hizes5050@gmail.com>
2026-03-20 18:53:51 +0100

    handleCardClickLogic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { GameHeader } from './components/GameHeader';
+import { Card } from './components/Card';
+import './index.css';
+import { useEffect, useState } from 'react';
 
 const cardValues = [
   '🍎',
@@ -20,14 +23,86 @@ const cardValues = [
 ];
 
 function App() {
-  return ( 
+  const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [mathcedCards, setMatchedCards] = useState([]);
+
+  const initializeGame = () => {
+    const finalCards = cardValues.map((value, index) => ({
+      id: index,
+      value,
+      isFlipped: false,
+      isMatched: false,
+    }));
+
+    setCards(finalCards);
+  };
+
+  useEffect(() => {
+    initializeGame();
+  }, []);
+
+  const handleCardClick = (card) => {
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+
+    const newFlippedCards = [...flippedCards, card.id];
+    setFlippedCards(newFlippedCards);
+
+    if (flippedCards.length === 1) {
+      const firstCard = cards[flippedCards[0]];
+
+      if (firstCard.value === card.value) {
+        setTimeout(() => {
+          setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
+
+          setCards((prevCards) =>
+            prevCards.map((c) => {
+              if (c.id === card.id || c.id === firstCard.id) {
+                return { ...c, isMatched: true, isFlipped: false };
+              }
+              return c;
+            }),
+          );
+
+          setFlippedCards([]);
+        }, 500);
+      } else {
+        setTimeout(() => {
+          const flipBackCard = newCards.map((c) => {
+            if (newFlippedCards.includes(c.id)) {
+              return { ...c, isFlipped: false };
+            } else {
+              return c;
+            }
+          });
+          setCards(flipBackCard);
+
+          setFlippedCards([]);
+        }, 1000);
+      }
+    }
+  };
+
+  return (
     <div className="app">
       <GameHeader score={3} moves={10} />
 
-      <div className='cards-grid'>
-        {cardValues.map((card) => {
-          
-        })}
+      <div className="cards-grid">
+        {cards.map((card) => (
+          <Card key={card.id} card={card} onClick={handleCardClick} />
+        ))}
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,10 @@
-export const Card = () => {};
+export const Card = ({ card, onClick }) => {
+  return (
+    <div
+      className={`card ${card.isFlipped ? 'flipped' : ''} ${card.isMatched ? 'matched' : ''}`}
+      onClick={() => onClick(card)}>
+      <div className="card-front"></div>
+      <div className="card-back">{card.value}</div>
+    </div>
+  );
+};
